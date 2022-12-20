86038386a3f41fc1db4796da965131ae931deedb
mahesa10 <pandu.mahesa9@gmail.com>
2022-12-20 08:41:22 +0700

    Add function to handle and display data

--- public/script/api.js
+++ public/script/api.js
@@ -2,14 +2,14 @@
 const fetchWeather = async(location) => {
   try {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ac0a1ad411df28a2b97905ff8a76b56d`, { mode: 'cors' });
-    console.log(response);
+    // console.log(response);
+
+    let weatherData = await response.json();
     
     if (!response.ok) {
-      throw response.statusText;
+      throw weatherData.message;
     }
-    
-    let weatherData = await response.json();
-    console.log(weatherData);
+
     return weatherData;
   } catch(err) {
     return err;
